5541088c5c4547087ff6c271c41ea33f3f1a5cd9
PhanTu2551993 <phanvantu25051993@gmail.com>
2024-08-09 16:58:54 +0700

    Add project files.

---
+++ 9.2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Play = Sony.Players.DvdPlayer;
+//using Play = Sony.Player; // Sửa lại alias cho đúng namespace
+
+namespace Sony
+{
+    //C2: Thêm sửa Player thành Players để giống với namespace đã được gọi 
+    namespace Players
+    {
+        class CdPlayer
+        {
+            public CdPlayer()
+            {
+                Console.WriteLine("CD-R as well as CD-RW are available");
+            }
+        }
+
+        //class DvdPlayer()
+        class DvdPlayer // Loại bỏ dấu ngoặc đơn () trong khai báo lớp
+        {
+            public DvdPlayer()
+            {
+                Console.WriteLine("DVD-R as well as DVD-RW are available");
+            }
+        }
+    }
+}
+
+namespace Samsung
+{
+    class Player
+    {
+        static void Main(string[] args)
+        {
+            //Play.DvdPlayer objPlayer = new Players.DvdPlayer();
+                Play objPlayer = new Play();  // Sử dụng alias Play để khởi tạo DvdPlayer
+            //Play.DvdPlayer objPlayer = new Play.DvdPlayer(); // Sử dụng đúng alias và từ khóa new
+        }
+    }
+}
---
+++ Chuong9/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Sony
+{
+    class Laptops
+    {
+        public Laptops()
+        {
+            Console.WriteLine("Manufactures Laptops of different configuarations");
+        }
+
+        // Lỗi class Player phải để ở public vì constructure của class Player đang để ở public
+        //class Player
+
+        //Lớp Player trong namespace Sony được định nghĩa nhưng không được sử dụng trong mã, điều này không gây ra lỗi khi chạy
+        //nhưng có thể là phần dư thừa trong đoạn code này hoặc xảy ra lỗi khi muốn sử dụng đến Player
+
+        //Sửa lỗi
+        public class Player
+        {
+            public Player()
+            {
+                Console.WriteLine("CD as well as DVD players are manufactures");
+            }
+        }
+    }
+}
+namespace Dell
+{
+    class Laptops
+    {
+        static void Main(string[] args)
+        {
+            Sony.Laptops objSonyLaptop = new Sony.Laptops();
+        }
+    }
+}
